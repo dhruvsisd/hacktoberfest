de5c2760a4f2e3e6196d2de52dd7985a9c7bd66b
Benny Schreck <benny.schreck@kanzlei-jacobs.de>
2018-10-09 13:33:05 +0200

    Oktoberfest answers

--- helloOctoberfest2018.java
+++ helloOctoberfest2018.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Octoberfest2018!");
+        System.out.println("Oktoberfest:  Hello User");
     }
 }
